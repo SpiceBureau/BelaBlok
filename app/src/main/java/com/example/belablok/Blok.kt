b95242c815bbd8620bed15b6cb73d06bb8bb6c42
SpiceBureau <sebesic.dominik@gmail.com>
2023-02-06 13:31:45 +0100

    initial commit

---
+++ app/src/main/java/com/example/belablok/Blok.kt
@@ -0,0 +1,11 @@
+package com.example.belablok
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Blok : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
