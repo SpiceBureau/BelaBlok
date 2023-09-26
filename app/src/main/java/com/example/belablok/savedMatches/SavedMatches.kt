8eb2ae44ff7d0ca825489e73e7351f6b16cff7ae
SpiceBureau <sebesic.dominik@gmail.com>
2023-09-26 14:06:12 +0200

    misc fixes

--- app/src/main/java/com/example/belablok/savedMatches/SavedMatches.kt
+++ app/src/main/java/com/example/belablok/savedMatches/SavedMatches.kt
@@ -3,19 +3,14 @@ package com.example.belablok.savedMatches
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ListView
-import android.widget.TextView
 import com.example.belablok.R
-import com.example.belablok.roundlist.RoundListAdapter
 import com.example.belablok.storage.MatchStorage
-import com.example.belablok.storage.User
-import com.example.belablok.storage.UserStorage
 
 class SavedMatches : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_saved_matches)
-
-        this.actionBar?.hide()
+        setContentView(R.layout.saved_matches_activity)
+        supportActionBar?.hide()
 
         val listView: ListView = findViewById(R.id.matchListView)
 
