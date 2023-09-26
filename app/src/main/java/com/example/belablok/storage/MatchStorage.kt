15dd0c0b357aac7f17a81d1182d3abfba03c2055
SpiceBureau <sebesic.dominik@gmail.com>
2023-09-26 12:16:28 +0200

    MatchStorage added

---
+++ app/src/main/java/com/example/belablok/storage/MatchStorage.kt
@@ -0,0 +1,37 @@
+package com.example.belablok.storage
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+
+class MatchStorage(context: Context) {
+    private val preferences: SharedPreferences
+    private val gson: Gson
+
+    init {
+        preferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+        gson = Gson()
+    }
+
+    fun saveData(data: List<List<User?>>?) {
+        val dataJson = gson.toJson(data)
+        preferences.edit().putString(DATA_KEY, dataJson).apply()
+    }
+
+    fun loadData(): List<List<User?>>? {
+        val dataJson = preferences.getString(DATA_KEY, null)
+        return if (dataJson != null) {
+            val dataType = object : TypeToken<List<List<User?>>?>() {}.type
+            gson.fromJson(dataJson, dataType)
+        } else {
+            ArrayList()
+        }
+    }
+
+    companion object {
+        private const val PREF_NAME = "MatchesData"
+        private const val DATA_KEY = "matches_data_key"
+    }
+}
